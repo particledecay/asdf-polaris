40cadf1a0478bef6057a49d06dce470c7a312b26
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-10-22 00:11:28 -0400

    refactor: use new plugin template install
    Polaris changed the naming scheme for its downloads anyway, so a
    refactoring was needed. Might as well update this repo to using the
    newer asdf-plugin-template version of doing things.

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for polaris.
-GH_REPO="https://github.com/FairwindsOps/polaris"
+GH_NAME="FairwindsOps/polaris"
+GH_REPO="https://github.com/${GH_NAME}"
+GH_API="https://api.github.com/repos/${GH_NAME}"
 TOOL_NAME="polaris"
 TOOL_TEST="polaris version"
 
@@ -24,27 +25,64 @@ sort_versions() {
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
+get_os() {
+  uname | tr '[:upper:]' '[:lower:]'
+}
+
+get_arch() {
+  local -r arch=$(uname -m)
+
+  case $arch in
+    x86_64)
+      echo amd64
+      ;;
+    *)
+      echo $arch
+      ;;
+  esac
+}
+
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
     sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
+list_github_releases() {
+  local url
+  url="${GH_API}/releases?per_page=100"
+
+  curl "${curl_opts[@]}"  "$url" \
+    | grep tag_name \
+    | sed 's/"tag_name": //g;s/"//g;s/,//g'
+}
+
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if polaris has other means of determining installable versions.
-  list_github_tags
+  list_github_releases
 }
 
 download_release() {
-  local version filename url
+  local version filename url os arch legacy_tarball_version is_old_version
   version="$1"
   filename="$2"
-
-  # TODO: Adapt the release URL convention for polaris
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  os=$(get_os)
+  arch=$(get_arch)
+  legacy_tarball_version="4.0.5" # this version and prior have older naming scheme
+  is_old_version=$(echo $version $legacy_tarball_version \
+    | xargs -n 1 \
+    | sort_versions \
+    | sed "/${legacy_tarball_version}/q" \
+    | awk -v version=$version '$1 == version')
 
   echo "* Downloading $TOOL_NAME release $version..."
+  if [ -n "$is_old_version" ]
+  then
+    remote_file="${TOOL_NAME}_${version}_${os}_${arch}.tar.gz"
+  else
+    remote_file="${TOOL_NAME}_${os}_${arch}.tar.gz"
+  fi
+
+  url="$GH_REPO/releases/download/$version/$remote_file"
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
@@ -59,12 +97,12 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-
-    # TODO: Asert polaris executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+
+    cp -r "$ASDF_DOWNLOAD_PATH/$tool_cmd" "$install_path"
+
+    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
