a2586c4f0b3033655a3789a9188c58f101aace9a
Joey Espinosa <jlouis.espinosa@gmail.com>
2021-10-22 00:43:14 -0400

    fix(build): adjust build files for main branch

--- lib/utils.bash
+++ lib/utils.bash
@@ -96,13 +96,13 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
+    mkdir -p "$install_path/bin"
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 
-    cp -r "$ASDF_DOWNLOAD_PATH/$tool_cmd" "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH/$tool_cmd" "$install_path/bin"
 
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
